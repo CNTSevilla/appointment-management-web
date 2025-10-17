fdbc95ff7ab10ec557833f7988f3ad78641165cc
Jesús Daza <jesus.daza@solucionex.com>
2025-10-17 21:59:39 +0200

    refactor: update global config and form field model

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
-  ]
+    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }  ]
 };
+
--- src/app/core/models/form-field.model.ts
+++ src/app/core/models/form-field.model.ts
@@ -10,6 +10,7 @@ export type FieldType =
   | 'date'
   | 'datetime-local'
   | 'time'
+  | 'tel'
   | 'file';
 
 export interface FormField {
